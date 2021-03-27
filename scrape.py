5c0b039997475db31d651f6861228b1e9950706a
Erik <snowy@snowballs-MacBook-Pro.local>
2021-03-27 01:55:05 -0700

    Scrape into monthly aggregated files

--- scrape.py
+++ scrape.py
@@ -5,59 +5,52 @@ import urllib.request
 import requests
 from bs4 import BeautifulSoup
 
-def manage_index(index):
-    output = []
-    return output
-
 def parse_site(index=0):
+    if not os.path.exists('data/raw_month_data'):
+        os.makedirs('data/raw_month_data')
     base_url = 'http://www.nuforc.org/webreports/'
     starting_slug = 'ndxevent.html'
     output = []
-    # grab  months page
     months_page = requests.get(base_url + starting_slug).content
-    # scrape month urls from table
     month_urls = []
     soup_months = BeautifulSoup(months_page, 'html.parser')
     months = soup_months.find_all('td')
     for month in months:
         if month.find('a', href=True):
             month_urls.append(month.a['href'])
-    # get report urls from each month
-    report_urls = []
-    for month in month_urls[0:1]: # release me! ------------------------- * <----
+    for month in month_urls:
+        filename = 'data/raw_month_data/' + os.path.splitext(month)[0] + '.json'
+        file_index = []
+        if os.path.exists(filename):
+            with open(filename) as f:
+                file_contents = json.load(f)
+            for report in file_contents:
+                file_index.append(report['report_id'])
+        else:
+            file_contents = []
         reports_page = requests.get(base_url + month).content
         soup_reports = BeautifulSoup(reports_page, 'html.parser')
         table = soup_reports.find('tbody')
         a = table.find_all('a', href=True)
+        report_urls = []
         for href in a:
-            report_urls.append(href['href'])
-    # handle output file
-    # with open('raw_reports.json', 'w+') as raw_file:
-    #     if os.path.getsize('raw_reports.json') == 0:
-    #         reports_array = []
-    #     else:
-    #         reports_array = json.load(raw_file)
-    #     print(reports_array)
-    
-    for report in report_urls:
-        print(parse_report(base_url+report))
+            report_id = href['href'].rsplit('/', 1)[-1].split('.')[0]
+            if report_id not in file_index:
+                report_urls.append(href['href'])
+        for report in report_urls:
+            file_contents.append(parse_report(base_url+report))
+        with open(filename, 'w', encoding='utf-8') as f:
+            json.dump(file_contents, f, ensure_ascii=False, indent=4)
     
 
 def parse_report(url):
     report = {}
-    # grab report id from url
     report['report_id'] = url.rsplit('/', 1)[-1].split('.')[0]
-    # grab report page
-    raw_report_page = urllib.request.urlopen(url)
-    report_page_bytes = raw_report_page.read()
-    report_page = report_page_bytes.decode("utf8")
-    raw_report_page.close()
-    # scrape table
+    report_page = requests.get(url).content
     table_data = []
     soup_report = BeautifulSoup(report_page, 'lxml')
     table = soup_report.table
     rows = table.find_all('tr')
-    # scrape field values in upper tr
     fields = rows[1].td.font
     field_list = []
     for elem in fields.descendants:
@@ -71,8 +64,8 @@ def parse_report(url):
             else:
                 field = parse_field(elem)
                 report[field['category']] = field['content']
-    # scrape description in lower tr
-    report['description'] = rows[2].td.font.get_text()
+    description = rows[2].td.font.get_text()
+    report['description'] = description.replace('"', '\\"')
     return report
 
 def parse_field(input):
