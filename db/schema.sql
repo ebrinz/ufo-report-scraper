4ed564937545c610da9c26530c64b3bafd888769
ebrinz <email>
2024-11-21 15:27:12 -0800

    DB stuff

--- db/schema.sql
+++ db/schema.sql
@@ -1,20 +1,20 @@
 
 
 CREATE TABLE IF NOT EXISTS ufo_reports_raw (
-        report_id VARCHAR(255),
-        entered TEXT,
-        occurred TEXT,
-        reported TEXT,
-        posted TEXT,
-        location TEXT,
-        shape TEXT,
-        duration TEXT,
-        description TEXT,
-        status_code INT,
-        characteristics TEXT
-);
+    report_id VARCHAR(255),
+    entered TEXT,
+    occurred TEXT,
+    reported TEXT,
+    posted TEXT,
+    location TEXT,
+    shape TEXT,
+    duration TEXT,
+    description TEXT,
+    status_code INT,
+    characteristics TEXT
+)
 
-CREATE TABLE IF NOT EXISTS ufo_reports (
+CREATE TABLE IF NOT EXISTS ufo_reports_transform (
     report_id VARCHAR(255) PRIMARY KEY,
     entered TIMESTAMP,
     occurred TIMESTAMP,
@@ -23,18 +23,17 @@ CREATE TABLE IF NOT EXISTS ufo_reports (
     location TEXT,
     shape TEXT,
     duration INT,
-    description TEXT,
-    status_code INT,
-    characteristics TEXT
-);
+    description TEXT[],
+)
 
 CREATE TABLE IF NOT EXISTS description_sentence_embeddings (
     report_id VARCHAR(255) PRIMARY KEY,
     sentence_id VARCHAR(255),
     embedding VECTOR(128),
-);
+)
 
 CREATE TABLE IF NOT EXISTS description_averaged_embeddings (
     report_id VARCHAR(255) PRIMARY KEY,
     embedding VECTOR(128),
-);
+)
+;
