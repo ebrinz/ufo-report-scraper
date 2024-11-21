5c0ce17bbdde0fa75ecef9a0942af7fd8fcde897
ebrinz <email>
2024-11-21 13:06:59 -0800

    Erase empty lines

--- src/extract_archived_data.py
+++ src/extract_archived_data.py
@@ -2,13 +2,10 @@ import tarfile
 import os
 
 def extract_tar(tar_path, extract_to):
-
     if not os.path.exists(tar_path):
         print(f"Error: File '{tar_path}' does not exist.")
         return
-    
     os.makedirs(extract_to, exist_ok=True)
-
     try:
         with tarfile.open(tar_path, 'r') as tar:
             print(f"Extracting files from '{tar_path}' to '{extract_to}'...")
@@ -21,5 +18,4 @@ def extract_tar(tar_path, extract_to):
 if __name__ == "__main__":
     tar_file_path = "data/archive/nuforc_dataset.tar" 
     output_directory = "data/raw" 
-
     extract_tar(tar_file_path, output_directory)
